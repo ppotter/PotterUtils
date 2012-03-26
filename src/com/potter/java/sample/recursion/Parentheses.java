96fb1f9bcd9b67626c65c184ad82a7d23019dea5
Patrick Potter <patrick.potter2@gmail.com>
2012-03-26 15:55:09 -0400

    Altering the Recursive Parentheses Algorithm to a more correct and simple variation.

--- src/com/potter/java/sample/recursion/Parentheses.java
+++ src/com/potter/java/sample/recursion/Parentheses.java
@@ -1,41 +1,27 @@
 package com.potter.java.sample.recursion;
 
 /*
- * algorithm to print all valid (e g , properly opened and closed) 
- * combinations of n-pairs of parentheses 
+ * Class designed to output all the possible valid pairs of Parentheses. 
  */
 
 public class Parentheses {
-
-	public static String parenthesesStarter(int n){
-		StringBuilder results = new StringBuilder();
-		if(n > 1){
-
-			for(int j = 0; j < n; j++){
-				results.append(parentheses(1));
-			}
-			results.append(",");
+	static void parentheses(int open, int close, String result) {
+		if (open == 0 && close == 0) {
+			//recursion has reached the end of a pair(hit the maximum number of parentheses
+			//it can use, So output the built string.
+			System.out.println(result);
 		}
-		if(n > 2){
-			for(int i = n-1; i > 0; i--){
-				results.append(parentheses(i));
-				results.append(parentheses(n-i));
-				results.append(",");
-			}
+		//if you have more open parentheses to use then use them, decrement the amount of
+		// available openers and increment the amount of available closers.
+		if (open > 0) {
+			parentheses(open-1, close+1, result + "(");
 		}
-		results.append(parentheses(n));
-		return results.toString();
-	}
-	
-	public static String parentheses(int n){
-		if(n <= 1){
-			return "()";
+		//if you have more close parentheses available then use them and decrement that amount.
+		if (close > 0) {
+			parentheses(open, close-1, result + ")");
 		}
-		return "(" + parentheses(n-1) + ")";
 	}
-	
-	public static void main(String[] args){
-		System.out.println(parenthesesStarter(3));
+	public static void main(String[] args) {
+		parentheses(4, 0, "");
 	}
-	
 }
